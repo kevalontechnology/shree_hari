3c1658fe371fe6614914702da4d54882474ae033
Jeel Popat <jeelpopat1010@gmail.com>
2026-08-01 14:56:45 +0530

    Backend

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   // Hardcoded to ensure it works immediately on Render without env var configuration issues
   // baseURL: 'https://shree-hari-backend-9ww2.onrender.com/api',
-  baseurl: 'https://shree-hari-backend-9ww2.onrender.com'
+  baseURL: 'https://shree-hari-bakendx.onrender.com/api'
 });
 
 export default api;

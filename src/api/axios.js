1a3a6d5951f4966ac2328d8de2c8759692bd794b
Jeel Popat <jeelpopat1010@gmail.com>
2026-08-01 13:11:22 +0530

    Initial commit

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
   // Hardcoded to ensure it works immediately on Render without env var configuration issues
-  baseURL: 'https://shree-hari-backend-9ww2.onrender.com/api',
+  baseURL: 'http://localhost:5000/api',
+  
 });
 
 export default api;
